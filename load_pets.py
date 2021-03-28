6a1b774887871b6b332519f31eebac30ae2a279c
wilsonvetdev <wilsongch92@gmail.com>
2021-03-27 21:59:39 -0400

    main

--- load_pets.py
+++ load_pets.py
@@ -25,21 +25,22 @@ person_pet = (
     (4, 6)
 )
 
+if __name__ == '__main__':
 
-con = lite.connect('pets.db')
+    con = lite.connect('pets.db')
 
-with con:
-    cur = con.cursor()
+    with con:
+        cur = con.cursor()
 
-    cur.executescript('''
-    DROP TABLE IF EXISTS person;
-    DROP TABLE IF EXISTS pet;
-    DROP TABLE IF EXISTS person_pet;
-    CREATE TABLE person(id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, age INTEGER);
-    CREATE TABLE pet (id INTEGER PRIMARY KEY, name TEXT, breed TEXT, age INTEGER, dead INTEGER);
-    CREATE TABLE person_pet (person_id INTEGER, pet_id INTEGER);
-    ''')
+        cur.executescript('''
+        DROP TABLE IF EXISTS person;
+        DROP TABLE IF EXISTS pet;
+        DROP TABLE IF EXISTS person_pet;
+        CREATE TABLE person(id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, age INTEGER);
+        CREATE TABLE pet (id INTEGER PRIMARY KEY, name TEXT, breed TEXT, age INTEGER, dead INTEGER);
+        CREATE TABLE person_pet (person_id INTEGER, pet_id INTEGER);
+        ''')
 
-    cur.executemany('INSERT INTO person VALUES(?, ?, ?, ?)', people)
-    cur.executemany('INSERT INTO pet VALUES(?, ?, ?, ?, ?)', pets)
-    cur.executemany('INSERT INTO person_pet VALUES(?, ?)', person_pet)
+        cur.executemany('INSERT INTO person VALUES(?, ?, ?, ?)', people)
+        cur.executemany('INSERT INTO pet VALUES(?, ?, ?, ?, ?)', pets)
+        cur.executemany('INSERT INTO person_pet VALUES(?, ?)', person_pet)
